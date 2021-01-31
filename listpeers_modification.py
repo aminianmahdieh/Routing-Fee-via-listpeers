2b5f1f1c6a0d09dd7bca6cbccc89bed3c14d188f
Mahdieh <72521500+aminianmahdieh@users.noreply.github.com>
2021-01-31 22:54:00 +0100

    Add files via upload

---
+++ listpeers_modification.py
@@ -0,0 +1,33 @@
+from lightning import Plugin
+import json
+
+plugin = Plugin()
+
+@plugin.method("listpeers_modification")
+def listpeers_modification(plugin):
+
+    reply = {}
+    peers = plugin.rpc.listpeers()
+    channelss= plugin.rpc.listchannels()
+    fee_rate= 0.000001
+    for p in peers['peers']:
+        for c in p['channels']:
+            short_channel_id= c['short_channel_id']
+            for channel in channelss['channels']:
+                #from listchannels we find the base fee for each channel
+                #based on channels short id
+                if channel['short_channel_id']==short_channel_id:
+                    base_fee = int(channel['base_fee_millisatoshi'])*0.0001
+                    break
+                routing_fee = base_fee + fee_rate
+                c['routing_fees'] = routing_fee
+    reply['channels'] = json.dumps(peers)
+    return reply
+
+
+@plugin.init()
+def init(options, configuration, plugin):
+    plugin.log("Plugin listpeers_modification.py initialized")
+
+
+plugin.run()
